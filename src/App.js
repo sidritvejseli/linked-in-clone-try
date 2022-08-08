efa04f22fe61b58a5228100436525e80a95da599
sidritvejseli <sidrit2016@gmail.com>
2022-08-08 19:54:27 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Sidrit</code> and save to reload.
+          Edit <code>Sidrit after commit</code> and save to reload.
         </p>
         <a
           className="App-link"
